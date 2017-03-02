16f802eebf6fb1209c8f72e4a01528cf308fa1e1
r3dDoX <patrick@walther-online.ch>
2017-03-02 09:56:50 +0100

    change to environment url for production

--- server/redisClient.js
+++ server/redisClient.js
@@ -1,4 +1,9 @@
 const config = require('config');
 const redis = require('redis');
 
-module.exports = redis.createClient({host: config.get('redisHost'), port: config.get('redisPort')});
+if (config.get('redisUrl')) {
+    module.exports = redis.createClient({ url: config.get('redisUrl') });
+} else {
+    module.exports = redis.createClient({ host: config.get('redisHost'), port: config.get('redisPort') });
+}
+
