7a0eec89cb7606585b912aab32f71d67627e2793
r3dDoX <patrick@walther-online.ch>
2017-03-02 11:04:51 +0100

    switch to redis connection by url

--- server/redisClient.js
+++ server/redisClient.js
@@ -1,9 +1,8 @@
 const config = require('config');
 const redis = require('redis');
 
-if (config.get('redisUrl')) {
-    module.exports = redis.createClient({ url: config.get('redisUrl') });
-} else {
-    module.exports = redis.createClient({ host: config.get('redisHost'), port: config.get('redisPort') });
-}
+const client = redis.createClient({ url: config.get('redisUrl') });
 
+client.on('error', err => console.error(err));
+
+module.exports = client;
