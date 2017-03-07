b633ce889aab811a242bbc0da956e60bac793fed
r3dDoX <patrick@walther-online.ch>
2017-03-07 10:44:07 +0100

    shorten syntax

--- client/js/nav/AppNavbar.js
+++ client/js/nav/AppNavbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import NavbarHeader from 'react-bootstrap/lib/NavbarHeader';
 import NavbarBrand from 'react-bootstrap/lib/NavbarBrand';
@@ -10,35 +10,28 @@ import NavbarCollapse from 'react-bootstrap/lib/NavbarCollapse';
 import LoggedOutButtons from './LoggedOutButtons';
 import LoggedInButtons from './LoggedInButtons';
 
-const AppNavbar = ({isTransparent, isAuthenticated}) => {
-
-    const buttons = isAuthenticated ? <LoggedInButtons/> : <LoggedOutButtons/>;
-
-    return (
-        <Navbar fixedTop inverse className={isTransparent ? 'transparent' : ''}>
-            <NavbarHeader>
-                <NavbarBrand>
-                    <Link to="/">Jass-Challenge</Link>
-                </NavbarBrand>
-                <NavbarToggle />
-            </NavbarHeader>
-            <NavbarCollapse>
-                {buttons}
-            </NavbarCollapse>
-        </Navbar>
-    );
-};
+const AppNavbar = ({ isTransparent, isAuthenticated }) => (
+    <Navbar fixedTop inverse className={isTransparent ? 'transparent' : ''}>
+        <NavbarHeader>
+            <NavbarBrand>
+                <Link to="/">Jass-Challenge</Link>
+            </NavbarBrand>
+            <NavbarToggle />
+        </NavbarHeader>
+        <NavbarCollapse>
+            {isAuthenticated ? <LoggedInButtons /> : <LoggedOutButtons />}
+        </NavbarCollapse>
+    </Navbar>
+);
 
 AppNavbar.propTypes = {
     isTransparent: React.PropTypes.bool,
     isAuthenticated: React.PropTypes.bool,
 };
 
-const mapStateToPros = (state) => ({
-    isAuthenticated: state.user.isAuthenticated,
-});
-
 export default connect(
-    mapStateToPros,
+    (state) => ({
+        isAuthenticated: state.user.isAuthenticated,
+    }),
     {}
 )(AppNavbar);
